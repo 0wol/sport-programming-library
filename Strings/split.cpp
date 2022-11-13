931fd5d2d22e4698eb72d5e707fcfa6865f0b85c
Glushin Nikita <nikita.glushin@yandex.ru>
2022-11-13 19:29:39 +0300

    split function for string

---
+++ Strings/split.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+
+std::vector<std::string> split(std::string s, char delimeter) {
+	std::vector<std::string> v;
+	int last = 0;
+	while(last < s.size() && s[last] == delimeter) last++;
+	for (int i = last; i < s.size(); ++i) {
+		if (s[last] == delimeter) {
+			last = i;
+			continue;
+		}
+		if (s[i] == delimeter) {
+			v.push_back(s.substr(last, i - last));
+			last = i + 1;
+		}
+	}
+	
+	if (last < s.size() && s[last] != delimeter)
+		v.push_back(s.substr(last, s.size() - last));
+		
+	return v;
+}
